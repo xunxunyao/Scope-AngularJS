84f99b7ffddbc9421132cf738d46a960998bb45a
yaoxunxun <xunxunyao@gmail.com>
2017-04-04 11:56:48 +0800

    dirty-checking

--- src/scope.js
+++ src/scope.js
@@ -7,23 +7,30 @@ function Scope() {
     //存储所有被注册的监听器
     //$$在AngularJS中哦被认为是私有变量，不应该被外部调用
     this.$$watchers = [];
-    //想要每一个Scope对象都有$watch函数，将它添加到Scope原型中；
-    //todo $watch就是监听对象，将进行相应操作的listenerFn存储起来
-    Scope.prototype.$watch = function (watchFn, listenerFn) {
-        var watcher = {
-            watchFn: watchFn,
-            listenerFn: listenerFn
-        };
-        this.$$watchers.unshift(watcher);//将监听器存储到$$watchers数组，添加到数组开头
-    };
-    //todo $digest就是调用所有的监听函数listenerFn
-    Scope.prototype.$digest = function() {
-        var length = this.$$watchers.length;
-        var watcher;
-        //注意到我们在开始时正向添加监视器数组然后逆序迭代它。这样的做法将会让我们在实现移除监视器时轻松一点
-        while (length--) {
-            watcher = this.$$watchers[length];
-            watcher.listenerFn();
-        }
-    };
 }
+//想要每一个Scope对象都有$watch函数，将它添加到Scope原型中；
+//todo $watch就是返回变化的数据，将进行相应操作的listenerFn存储起来
+Scope.prototype.$watch = function (watchFn, listenerFn) {
+    var watcher = {
+        watchFn: watchFn,
+        listenerFn: listenerFn
+    };
+    this.$$watchers.unshift(watcher);//将监听器存储到$$watchers数组，添加到数组开头
+};
+//todo $digest就是判断是否有变化，调用所有的监听函数listenerFn
+//$digest函数的职责是调用监视函数并将它的返回值与上一次的返回值进行对比。
+//如果两次的返回值不同，那么监视器就是dirty的，并且他的监听器函数应该被调用。
+Scope.prototype.$digest = function () {
+    var length = this.$$watchers.length;
+    var watcher, newValue, oldValue;
+    //注意到我们在开始时正向添加监视器数组然后逆序迭代它。这样的做法将会让我们在实现移除监视器时轻松一点
+    while (length--) {
+        watcher = this.$$watchers[length];
+        newValue = watcher.watchFn(this);
+        oldValue = watcher.last;
+        if(newValue !== oldValue){
+            watcher.last = newValue;
+            watcher.listenerFn(newValue, oldValue, this);
+        }
+    }
+};
--- test/scope_spec.js
+++ test/scope_spec.js
@@ -24,6 +24,36 @@ describe('Scope',function () {
             scope.$watch(watchFn,listenerFn);
             scope.$digest();
             expect(listenerFn).toHaveBeenCalled();//，$digest执行了以后，listenerFn这个函数是否有被调用
-        })
+        });
+        it("calls the watch function with the scope as the argument",function(){
+            var watchFn = jasmine.createSpy();
+            var listenerFn = function(){};
+            scope.$watch(watchFn,listenerFn);
+
+            scope.$digest();
+
+            expect(watchFn).toHaveBeenCalledWith(scope);
+        });
+        it("calls the listener function when the watched value changes", function() {
+            scope.someValue = 'a';
+            scope.counter = 0;
+            scope.$watch(
+                //监视函数是返回变化的数据，这个数据是存在scope中的某个东西
+                function(scope) { return scope.someValue; },
+                //每次发生变化，计数器的值就+1
+                //有新值和旧值可以方便的直到发生了什么变化
+                function(newValue, oldValue, scope) { scope.counter++; }
+            );
+            expect(scope.counter).toBe(0);
+            scope.$digest();
+            expect(scope.counter).toBe(1);
+            scope.$digest();
+            expect(scope.counter).toBe(1);
+            scope.someValue = 'b';
+            expect(scope.counter).toBe(1);
+            scope.$digest();
+            expect(scope.counter).toBe(2);
+        });
+
     })
 });
